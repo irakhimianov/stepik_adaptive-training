864e6006592de5d4b8eb5f804c6fb3995c6e928b
rkjar <rkjar@yandex.ru>
2022-01-24 23:43:39 +0500

    Адаптивный тренажер Python. Выполнение практических задач https://stepik.org/course/431/

--- 4_3.py
+++ 4_3.py
@@ -5,7 +5,7 @@ output = ""
 for ind, val in enumerate(my_str):
     if val in pattern:
         new_index = pattern.index(val) + shifr
-        if abs(new_index) > 27:
+        if abs(new_index) >= 26:
             new_index %= 27
         output += pattern[new_index]
-print(output)
+print(f'Result: "{output}"')
---
+++ x03.py
@@ -0,0 +1,2 @@
+import sys
+print(" ".join([el for el in sys.argv[1:]]))
